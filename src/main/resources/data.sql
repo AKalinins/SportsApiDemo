b3fc2be38f79aa19f0342608ff410f47c6e200fb
Aleksandrs Kalinins <aleksandrs.kalinins@28stone.com>
2024-12-17 12:04:30 +0200

    Add db scripts

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+-- This scripts will run automatically when app starts to populate db with some test data
+INSERT INTO events(name, type, status, start_time) VALUES('IIHF Hockey World Championship 2025', 'Hockey', 'INACTIVE', '2025-05-01 19:00');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE events
+(
+    id   bigint PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    type varchar(255) NOT NULL,
+    status varchar(255) NOT NULL,
+    start_time timestamp NOT NULL
+);
